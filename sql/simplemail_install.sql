7b0f0ed3efffee4117fb14b02b17cb508efdbba5
Robin Mitra <robin@compucorp.co.uk>
2014-07-24 15:51:34 +0100

    Add DAO and install/uninstall SQL for the mailing wizard entity

--- sql/simplemail_install.sql
+++ sql/simplemail_install.sql
@@ -2,6 +2,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `civicrm_simplemailheader`;
 DROP TABLE IF EXISTS `civicrm_simplemailmessage`;
+DROP TABLE IF EXISTS `civicrm_simplemail`;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -44,6 +45,43 @@ CREATE TABLE `civicrm_simplemailmessage` (
   `is_active` TINYINT      NOT NULL  DEFAULT 1
   ,
   PRIMARY KEY (`id`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_unicode_ci;
+
+-- /*******************************************************
+-- *
+-- * civicrm_simplemail
+-- *
+-- * Campaign details for Simple Mail
+-- *
+-- *******************************************************/
+CREATE TABLE `civicrm_simplemail` (
+  `id`               INT UNSIGNED     NOT NULL AUTO_INCREMENT COMMENT 'Unique SimpleMail ID',
+  `name`             VARCHAR(50)      NOT NULL
+  COMMENT 'Name of the mailing campaign',
+  `resume_step`      TINYINT UNSIGNED NOT NULL  DEFAULT 1
+  COMMENT 'The step to resume from on the wizard',
+  `header_id`        INT UNSIGNED     NULL
+  COMMENT 'The ID of a mailing header',
+  `subject`          VARCHAR(50)      NOT NULL
+  COMMENT 'Subject of the email',
+  `title`            VARCHAR(50)      NOT NULL
+  COMMENT 'Title/strapline for the email (in the title region)',
+  `body`             TEXT             NOT NULL
+  COMMENT 'Body of the email',
+  `message_id`       INT UNSIGNED     NULL
+  COMMENT 'The ID of the campaign message',
+  `send_immediately` TINYINT          NOT NULL  DEFAULT 0,
+  `send_on`          DATETIME         NULL
+  COMMENT 'Date scheduled for sending emails'
+  ,
+  PRIMARY KEY (`id`)
+
+  , CONSTRAINT FK_civicrm_simplemail_header_id FOREIGN KEY (`header_id`) REFERENCES `civicrm_simplemailheader` (`id`)
+  ON DELETE SET NULL, CONSTRAINT FK_civicrm_simplemail_message_id FOREIGN KEY (`message_id`) REFERENCES `civicrm_simplemailmessage` (`id`)
+  ON DELETE SET NULL
 )
   ENGINE =InnoDB
   DEFAULT CHARACTER SET utf8
--- sql/simplemail_uninstall.sql
+++ sql/simplemail_uninstall.sql
@@ -2,5 +2,6 @@ SET FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS `civicrm_simplemailheader`;
 DROP TABLE IF EXISTS `civicrm_simplemailmessage`;
+DROP TABLE IF EXISTS `civicrm_simplemail`;
 
 SET FOREIGN_KEY_CHECKS=1;
