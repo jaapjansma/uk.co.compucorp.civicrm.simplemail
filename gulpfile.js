28172b12ac8223745d9936245922dc8296ab772d
Robin Mitra <robinmitra1@gmail.com>
2015-09-14 11:59:38 +0100

    CATL-506: Update gulpfile to use the native gulp-sass instead of the wrapper gulp-ruby-sass, and update the npm package version.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
@@ -9,15 +10,14 @@ var notify = require('gulp-notify');
  * CSS operations
  */
 gulp.task('css', function () {
-
-    // the change to the gulpfile is due to this:
-    // http://stackoverflow.com/questions/28140012/gulp-typeerror-arguments-to-path-join-must-be-strings
-
-  return sass('css/src/style.scss', {style: 'compressed', sourcemap: true})
-    //.pipe(sass({style: 'compressed', sourcemap: true}))
+  return gulp.src('css/src/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
     .pipe(autoprefixer())
+    .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest('css/dist'))
-    .pipe(notify({message: 'CSS tasks complete!'}));
+    .pipe(notify({message: 'CSS tasks complete.'}));
 });
 
 gulp.task('javascript', function () {
@@ -28,10 +28,11 @@ gulp.task('javascript', function () {
     .pipe(notify({message: 'JavaScript tasks complete'}));
 });
 
-gulp.task('javascript-debug', function(){
+gulp.task('javascript-debug', function () {
   return gulp.src('js/src/*.js')
     .pipe(concat('script.js'))
-    .pipe(gulp.dest('js/dist'));
+    .pipe(gulp.dest('js/dist'))
+    .pipe(notify({message: 'JavaScript tasks complete'}));
 });
 
 /**
@@ -43,7 +44,7 @@ gulp.task('watch', function () {
 });
 
 
-gulp.task('watch-debug', function(){
+gulp.task('watch-debug', function () {
   gulp.watch('js/src/*.js', ['javascript-debug']);
 });
 
